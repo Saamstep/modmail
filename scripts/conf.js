b144d27ced06bf7372a241d668c3d23906bca302
Samstep.mac <Samstep2012@gmail.com>
2020-02-08 13:42:02 -0800

    progress on scripts

--- scripts/conf.js
+++ scripts/conf.js
@@ -4,7 +4,7 @@ VARS
 ==================
 */
 const fs = require("fs");
-const CONFIG_PATH = "./config.json";
+const CONFIG_PATH = "./tst.json";
 const colors = require("colors");
 const fetch = require("node-fetch");
 const readline = require("readline");
@@ -19,7 +19,7 @@ Start
 ==================
 */
 
-console.log(`\n\n\n\n\nModmail Config Generator`.blue);
+console.log(`\n\nYModmail Config Generator`.blue + "\n\n");
 
 /* 
 =================
@@ -27,35 +27,61 @@ FUNCTIONS
 ==================
 */
 
-function askForOverwrite() {
-  rl.question("Config file already exists. Overwrite? [Y]/[N]\n".red, function(response) {
-    switch (response) {
-      case "Y":
-        createConf();
-        break;
-      case "N":
-        rl.close();
-        break;
-      default:
-        askForOverwrite();
-    }
-  });
+async function askForOverwrite() {
+  const question1 = () => {
+    return new Promise((resolve, reject) => {
+      rl.question(`A config file already exists at \'${CONFIG_PATH}\' override the current config file?`.blue + `\n* NOTE: THIS IS IRREVERSABLE`.red.bold + `\n[Y]es or [N]o\n`.blue, answer => {
+        // console.log(`Thank you for your valuable feedback: ${answer}`);
+        switch (answer) {
+          case "Y":
+            createConf();
+            break;
+          case "N":
+            rl.close();
+            break;
+          default:
+            askForOverwrite();
+        }
+        resolve();
+      });
+    });
+  };
+  await question1();
 }
 
 async function createConf() {
+  console.log("Grabbing config from https://github.com/Saamstep/modmail/master/example.config.json...".green);
   const exampleconf = await fetch("https://raw.githubusercontent.com/Saamstep/modmail/master/example.config.json");
-  const data = await exampleconf.text();
-  console.log("Overwriting...".red);
-  fs.writeFileSync("tst.json", data);
-  modifyProps();
+  const data = await exampleconf.json();
+  const setToken = () => {
+    return new Promise((resolve, reject) => {
+      rl.question("Paste the client ID of the bot user from https://discordapp.com/developers/applications/\n".blue, answer => {
+        data.token = answer;
+        resolve();
+      });
+    });
+  };
+  const setOwner = () => {
+    return new Promise((resolve, reject) => {
+      rl.question("Paste the owner ID of the user\n".blue, answer => {
+        data.ownerid = answer;
+        resolve();
+      });
+    });
+  };
+  await setToken();
+  await setOwner();
+  console.log("Creating config file...".green);
+  fs.writeFileSync(CONFIG_PATH, JSON.stringify(data, null, 2));
+  rl.close();
 }
 
-function modifyProps() {
-  const conf = require("../tst.json");
-  for (i in conf) {
-    console.log(i);
-  }
-}
+// function modifyProps() {
+//   const conf = require("../tst.json");
+//   for (i in conf) {
+//     console.log(i);
+//   }
+// }
 
 /* 
 =================
@@ -63,8 +89,6 @@ LOGIC
 ==================
 */
 
-// createConf();
-// modifyProps();
 fs.access(CONFIG_PATH, fs.constants.F_OK, err => {
   if (err == null) {
     //already exists
@@ -76,6 +100,6 @@ fs.access(CONFIG_PATH, fs.constants.F_OK, err => {
 });
 
 rl.on("close", function() {
-  console.log("Readline closed");
+  console.log("Done!".green);
   process.exit(0);
 });
